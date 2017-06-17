4172a566c6f7e91806adc582e96424ab609ffb2b
qinghui <910944453@qq.com>
2017-06-17 23:24:53 +0800

    测试Jenkins

---
+++ AnewB_Eureka_Server/src/main/java/org/AnewB_Eureka_Server/App.java
@@ -0,0 +1,13 @@
+package org.AnewB_Eureka_Server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
