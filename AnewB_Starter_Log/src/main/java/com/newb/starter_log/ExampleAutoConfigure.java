ddf5457866fff7053f6e5b1423e5a332806132ae
qinghui <910944453@qq.com>
2018-12-12 15:04:36 +0800

    fix bug

--- AnewB_Starter_Log/src/main/java/com/newb/starter_log/ExampleAutoConfigure.java
+++ AnewB_Starter_Log/src/main/java/com/newb/starter_log/ExampleAutoConfigure.java
@@ -14,6 +14,12 @@ import org.springframework.context.annotation.Configuration;
  * @Deseription
  * https://docs.spring.io/spring-boot/docs/1.5.2.RELEASE/reference/htmlsingle/#boot-features-bean-conditions
  */
+
+/**
+ * 1.Spring Boot在启动时扫描项目所依赖的JAR包，寻找包含spring.factories文件的JAR包
+ * 2.根据spring.factories配置加载AutoConfigure类
+ * 3.根据 @Conditional注解的条件，进行自动配置并将Bean注入Spring Context
+ */
 @Configuration
 @ConditionalOnClass(ExampleService.class)//当classpath下发现该类的情况下进行自动配置。
 @EnableConfigurationProperties(ExampleServiceProperties.class)
